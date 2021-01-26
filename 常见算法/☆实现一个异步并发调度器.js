32cc46e28a311842b3790c470af823b5dee219b6
zhangtao <zhangtao15@xiaomi.com>
2021-01-26 22:05:38 +0800

    feat: 提交

---
+++ 常见算法/☆实现一个异步并发调度器.js
@@ -0,0 +1,31 @@
+//支持并发的调度器， 最多允许2两任务进行处理
+
+class Scheduler {
+ constructor(limit) {
+  this.limit = limit;
+  this.number = 0;
+  this.queue = [];
+ }
+ addTask(timeout, str) {
+  this.queue.push([timeout, str]);
+ }
+ start() {
+  if (this.number < this.limit && this.queue.length) {
+   var [timeout, str] = this.queue.shift();
+   this.number++;
+   setTimeout(() => {
+    console.log(str);
+    this.number--;
+    this.start();
+   }, timeout * 1000);
+   this.start();
+  }
+ }
+}
+
+const scheduler = new Scheduler(2);
+scheduler.addTask(1, "1"); // 1s后输出’1'
+scheduler.addTask(2, "2"); // 2s后输出’2'
+scheduler.addTask(1, "3"); // 2s后输出’3'
+scheduler.addTask(1, "4"); // 3s后输出’4'
+scheduler.start();
