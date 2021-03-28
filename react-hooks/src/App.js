9845419db300ffebcf27986c16ed465936398b17
z6t <1037457278@qq.com>
2021-03-28 11:06:37 +0800

    fix: 测试git config user.name

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 function App() {
   // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
 
   return (
     <div>
